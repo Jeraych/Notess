15761e40900c13c78f52c2df9f892c308635b04e
Jeraych <Jacky2022332467@gmail.com>
2026-03-13 13:24:41 +1300

    Setup MongoDB

--- backend/server.js
+++ backend/server.js
@@ -1,17 +1,19 @@
 const express = require("express");
 const cors = require("cors");
+const connectDB = require("./mongo");
+
+require("dotenv").config();
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
+connectDB();
+
 app.get("/", (req, res) => {
   res.send("API running");
 });
 
-const PORT = 5000;
-
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
